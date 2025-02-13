1bc2858f8570d891c3869c17d8cc9a7d2fa47e74
Modesto <79379450+moymazzoni@users.noreply.github.com>
2025-02-13 16:17:47 -0500

    Updated error handler and main. More changes to come.

--- file_operations.py
+++ file_operations.py
@@ -1,1 +1,10 @@
 """MODULE file_operations.py keeps all file operations in safe, working order."""
+import os
+
+def locate_directory():
+    print('find the directory in \'settings.txt\'.')
+    credentials_array = os.path.join(os.getcwd(), 'settings.txt')
+    return credentials_array
+
+def check_files(credentials_array):
+    print('check the directory and locate files.')
