b7a48444313494c7fb23f8e294dc58b204cec858
Modesto <modestomazzoni@gmail.com>
2025-02-14 02:03:54 -0500

    Work on read_files() next.

--- file_operations.py
+++ file_operations.py
@@ -1,10 +1,39 @@
 """MODULE file_operations.py keeps all file operations in safe, working order."""
 import os
+from utilities import Color
+b, i, u, e = Color.BOLD, Color.ITALIC, Color.UNDERLINE, Color.END # Terminal text format.
+g, y, r, w = Color.GREEN, Color.YELLOW, Color.RED, Color.WHITE # Terminal color format.
+
 
 def locate_directory():
-    print('find the directory in \'settings.txt\'.')
-    credentials_array = os.path.join(os.getcwd(), 'settings.txt')
-    return credentials_array
+    """FUNCTION locate_directory() is a one-time use function. Finds the locally saved credentials folder (dir)."""
+    saved_file_location = os.path.join(os.getcwd(), 'settings.txt')
+    with open(saved_file_location, 'r', encoding='utf-8') as f:
+        directory = f.readline().replace('Credentials Directory: ', '').replace('\\', '/')[:-1]
+    return directory
 
-def check_files(credentials_array):
-    print('check the directory and locate files.')
+def refresh_files(directory):
+    """FUNCTION refresh_files() refreshes the known directory contents to ensure up-to-date data."""
+    directory_len = len(os.listdir(directory))
+    credentials_array = [] * directory_len
+    for filename in os.listdir(directory):
+        credentials_array.append(os.path.join(directory, filename))
+    return credentials_array, directory_len
+
+def display_files(directory, credentials_array, directory_len):
+    """FUNCTION validate_files() goes through the directory (credentials folder) and prints if items present are valid
+    or not. It will also print what items it is referring to"""
+    refresh_files(directory) # Double-checks if the credentials folder has been tampered with or not.
+    credentials_valid = 0
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f) and f.endswith('.txt'):
+            print(f'{g}{Color.BOLD}[✔] | {e}"{filename}" - {b}Valid!{e}')
+            credentials_valid += 1
+        else:
+            print(f'{r}{b}[✖] | {e}"{filename}" - {b}ISN\'T Valid!{e}')
+    print(f'Total files present: {directory_len} | Text files present: {credentials_valid}')
+    return directory, credentials_array, directory_len
+
+def read_files():
+    print()
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from file_operations import locate_directory, refresh_files, display_files
 from utilities import Color
-import error_handler
+import error_handler, terminal, gui
 b, i, u, e = Color.BOLD, Color.ITALIC, Color.UNDERLINE, Color.END # Terminal text format.
 g, y, r, w = Color.GREEN, Color.YELLOW, Color.RED, Color.WHITE # Terminal color format.
 
 def main():
     while True:
-        user_input = input(f'{w}>> Type {b}"Window"{e}{w} or {b}"Terminal"{e}{w} for the respective mode...\n  {u}>{e} ')
+        user_input = input(f'{w}{b}>> Type "Window"{e}{w} or {b}"Terminal"{e}{w} for the respective mode...\n  {u}>{e} ')
         if user_input.lower() in ['window', 'win']:
-            print(f'{w}>> Entering... {i}Window Mode{e}{w}.{e}')
-            return 'window'
+            print(f'{w}{b}>> Entering... {i}Window Mode{e}{w}.{e}')
+            gui.gui()
+            return
         elif user_input.lower() in ['terminal', 'term']:
-            print(f'{w}>> Entering... {i}Terminal Mode{e}{w}.{e}')
-            return 'terminal'
+            print(f'{w}{b}>> Entering... {i}Terminal Mode{e}{w}.{e}')
+            directory = locate_directory() # Looks for 'default' dir location.
+            credentials_array, directory_len = refresh_files(directory)
+            display_files(directory, credentials_array, directory_len)
+            terminal.terminal()
+            print(credentials_array)
         else:
             error_handler.error_catcher(0)
 
