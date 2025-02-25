930376a7dcfb37d3ca2968ed1e35893411eb0493
Modesto <79379450+moymazzoni@users.noreply.github.com>
2025-02-25 16:59:22 -0500

    Made directory variable global within scope.

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from utilities import *
 def main():
     while True:
         userInput = input(f'{w}{b}>> Type "Window"{e}{w} or {b}"Terminal"{e}{w} for the respective mode...\n  {u}>{e} ')
+        directory = locate_directory()  # Looks for 'default' dir location.
         if userInput.lower() in ['window', 'win']:
             print(f'{w}{b}>> Entering... {i}Window Mode{e}{w}.{e}')
             gui.gui()
             return
         elif userInput.lower() in ['terminal', 'term']:
             print(f'{y}{b}>> Entering... {i}Terminal Mode{e}{y}.{e}')
-            directory = locate_directory() # Looks for 'default' dir location.
             refresh_files(directory)
             display_files(directory) # Lists file options before entering terminal.
             terminal.terminal(directory)
