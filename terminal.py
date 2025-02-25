ac953f2f5820ac05e0948a118980b4cc8de970cc
Modesto <79379450+moymazzoni@users.noreply.github.com>
2025-02-25 17:00:19 -0500

    Added docstrings for functions that needed it.

--- terminal.py
+++ terminal.py
@@ -1,8 +1,9 @@
-"""MODULE terminal.py handles the terminal portion, including layered command lines."""
+"""MODULE terminal.py handles the terminal portion, including layered command lines (L1 and L2, file, commands)."""
 from terminal_commands import GlobalCommands
 from utilities import *
 
 def terminal(directory):
+    """FUNCTION terminal() handles the first layer of commands (before entering file)."""
     commands = GlobalCommands()
     key = 'terminal'
     currentFile = None
@@ -13,7 +14,7 @@ def terminal(directory):
             print(f'{y}{b}>> Exiting program...{e}')
             break
         if cmd == 'read':
-            print(f'{y}{b}>> Entered... {i}file commands.{e}')
+            print(f'{y}{b}>> Entered {i}file commands.{e}')
             userInput_parts = list(filter(None, userInput.split(' ')))
             if len(userInput_parts) > 1:
                 currentFile = userInput_parts[1]
@@ -21,13 +22,13 @@ def terminal(directory):
                 currentFile = None
             terminal_2(currentFile, directory, commands)
 
-
-"""FUNCTION terminal was so good that we just had to make a sequel."""
 def terminal_2(currentFile, directory, commands):
+    """FUNCTION terminal was so good that we just had to make a sequel."""
     key2 = 'terminal_2'
     while True:
-        userInput2 = input(f'{w}{b}>> [L2]{e}{w} - Enter a command (in "{currentFile}"):\n  {u}>{e} ')
+        userInput2 = input(f'{w}{b}>> [L2]{e}{w} - Enter a command:'
+                           f'\n     ↪ (in: "{currentFile}")\n  {u}>{e} ')
         cmd2 = commands.process_command(userInput2, directory, key2, currentFile)
         if cmd2 == 'exit':
-            print(f'{y}{b}>> Exiting... {i}file commands.{e}')
+            print(f'{y}{b}>> Exited {i}file commands.{e}')
             return
